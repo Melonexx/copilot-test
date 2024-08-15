56aa6535a69654ee0ac8f98b07d65483842e6baa
Eileen Ilsemann <106445085+Melonexx@users.noreply.github.com>
2024-08-15 15:29:16 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const greeting = document.getElementById("greeting");
+const time = document.getElementById("time");
+const hour = new Date().getHours();
+const minute = new Date().getMinutes().toString().padStart(2, "0");
+
+time.innerHTML = `${hour}:${minute}`;
+
+if (hour >= 6 && hour < 12) {
+  greeting.innerHTML = "Good morning!";
+  document.body.style.background = "#f7daec";
+} else if (hour >= 12 && hour < 18) {
+  greeting.innerHTML = "Good afternoon!";
+  document.body.style.background = "#ebccf7";
+} else if (hour >= 18 && hour < 24) {
+  greeting.innerHTML = "Good evening!";
+  document.body.style.background = "#cccff7";
+} else {
+  greeting.innerHTML = "Good night!";
+  document.body.style.background = "#ccebf7";
+}
